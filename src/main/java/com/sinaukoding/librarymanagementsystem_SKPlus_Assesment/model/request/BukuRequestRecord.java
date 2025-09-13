da8656899e43972fb3f5ca56b3368e01bd0add81
hariantoputro <hariantoputro@gmail.com>
2025-09-14 01:33:04 +0800

    pembuatan validasi pada Buku RequestRecord

---
+++ src/main/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/model/request/BukuRequestRecord.java
@@ -0,0 +1,17 @@
+package com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.request;
+
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.enums.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
+
+public record BukuRequestRecord(String id,
+                                  @NotBlank(message = "Nama tidak boleh kosong") String nama,
+                                  @NotBlank(message = "Deskripsi tidak boleh kosong") String deskripsi,
+                                  @NotNull(message = "Harga tidak boleh kosong") Double harga,
+                                  @NotNull(message = "Stok tidak boleh kosong") Integer stok,
+                                  @NotNull(message = "Status tidak boleh kosong") Status status,
+                                  @NotEmpty(message = "Gambar tidak boleh kosong") Set<String> listImage) {
+}
