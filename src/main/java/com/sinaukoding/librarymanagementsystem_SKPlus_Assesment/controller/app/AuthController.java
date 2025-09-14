6b1add6cad3f1f5aec096fece3010df5cf4684be
hariantoputro <hariantoputro@gmail.com>
2025-09-14 09:37:41 +0800

    pembuatan AuthController untuk login dan logout

---
+++ src/main/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/controller/app/AuthController.java
@@ -0,0 +1,31 @@
+package com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.controller.app;
+
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.config.UserLoggedInConfig;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.request.LoginRequestRecord;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.response.BaseResponse;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.service.app.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("login")
+    public BaseResponse<?> login(@RequestBody LoginRequestRecord request) {
+        return BaseResponse.ok(null, authService.login(request));
+    }
+
+    @GetMapping("logout")
+    public BaseResponse<?> logout(@AuthenticationPrincipal UserLoggedInConfig userLoggedInConfig) {
+        var userLoggedIn = userLoggedInConfig.getUser();
+        authService.logout(userLoggedIn);
+        return BaseResponse.ok("Berhasil logout", null);
+    }
+
+}
+
