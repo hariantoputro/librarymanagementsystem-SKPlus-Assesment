8bb94391647be5762e8344b861823845a78a8458
hariantoputro <hariantoputro@gmail.com>
2025-09-14 10:35:24 +0800

    pembuatan unit test UserService - add

---
+++ src/test/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/UserServiceTest.java
@@ -0,0 +1,29 @@
+package com.sinaukoding.librarymanagementsystem_SKPlus_Assesment;
+
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.enums.Role;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.enums.Status;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.request.UserRequestRecord;
+import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.service.managementuser.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void addUserTest() {
+        UserRequestRecord request = new UserRequestRecord(null,
+                "Admin",
+                "admin",
+                "admin@email.com",
+                "admin123",
+                Status.AKTIF,
+                Role.ADMIN
+        );
+        userService.add(request);
+    }
+}
