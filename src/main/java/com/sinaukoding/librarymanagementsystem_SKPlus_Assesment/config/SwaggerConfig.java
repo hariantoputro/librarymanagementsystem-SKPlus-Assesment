54084e0fc443c01f5e4e2d97226db3f6b7e48244
hariantoputro <hariantoputro@gmail.com>
2025-09-14 13:23:36 +0800

    contoh image buku

---
+++ src/main/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Meirino Harianto Putro",
+                        email = "hariantoputromail@gmail.com"
+                ),
+                description = "Swagger documentation for Aplikasi Library Management System",
+                title = "Swagger Aplikasi Library Management System",
+                version = "1.0.0",
+                license = @License(
+                        name = "Sinau Koding",
+                        url = "https://sinaukoding.co.id/"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Development",
+                        url = "http://localhost:8081/"
+                ),
+                @Server(
+                        description = "Staging",
+                        url = "http://localhost:8081/"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
