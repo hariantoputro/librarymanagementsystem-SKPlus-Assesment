bf8111bccdd8df5220795da9539477e9e65ed891
hariantoputro <hariantoputro@gmail.com>
2025-09-14 19:38:52 +0800

    add parameter default pageable

--- src/main/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/controller/managementuser/UserController.java
+++ src/main/java/com/sinaukoding/librarymanagementsystem_SKPlus_Assesment/controller/managementuser/UserController.java
@@ -4,6 +4,11 @@ import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.filter.Use
 import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.request.UserRequestRecord;
 import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.model.response.BaseResponse;
 import com.sinaukoding.librarymanagementsystem_SKPlus_Assesment.service.managementuser.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("user")
 @RequiredArgsConstructor
+@Tag(name = "User API")
 public class UserController {
 
     private final UserService userService;
@@ -35,6 +41,12 @@ public class UserController {
 
     @PostMapping("find-all")
     @PreAuthorize("hasRole('ADMIN')")
+    @Parameters({
+            @Parameter(name = "page", description = "Page Number", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "0"), required = true),
+            @Parameter(name = "size", description = "Size Per Page", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "10"), required = true),
+            @Parameter(name = "sort", description = "Sorting Data", in = ParameterIn.QUERY, schema = @Schema(type = "string", defaultValue = "modifiedDate,desc"), required = true)
+    })
+
     public BaseResponse<?> findAll(@PageableDefault(direction = Sort.Direction.DESC, sort = "modifiedDate") Pageable pageable,
                                    @RequestBody UserFilterRecord filterRequest) {
         return BaseResponse.ok(null, userService.findAll(filterRequest, pageable));
